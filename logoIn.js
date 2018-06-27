1836e0731f20ecc0dd28527f76c87894aafbeac5
goolpar <40024197+goolpar@users.noreply.github.com>
2018-06-27 15:37:56 +0800

    Add files via upload

---
+++ logoIn.js
@@ -0,0 +1,11 @@
+
+		$(document).ready(function(){
+			$(".img01").delay("200").fadeIn(300);
+			$(".img02").delay("400").fadeIn(300);
+			$(".img03").delay("600").fadeIn(300);
+			$(".img04").delay("800").fadeIn(300);
+			$(".img05").delay("1000").fadeIn(300);
+			$(".img06").delay("1200").fadeIn(300);
+			$(".img07").delay("1400").fadeIn(300);
+			});
+	
